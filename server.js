e5bb855fe75decdc9c466809392fbf5648095aca
Raul Sanchez <Raul@Rauls-iMac.local>
2018-12-01 08:52:04 -0700

    Created basic server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+// Body-parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Connecting mLab MongoDB 🔌
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+
+// Simple get request (for testing ONLY)
+app.get("/", (req, res) => res.send("Hello"));
+
+// 🔥 up the server port 1024
+const port = process.env.PORT || 1024;
+app.listen(port, () => console.log(`Server running on port ${port}`));
