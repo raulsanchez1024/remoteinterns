6fb02a0b15e1d470f88c98b5ef4159ce16478e52
Raul <raulsanchez1024@gmail.com>
2018-12-14 18:38:04 -0700

    Added production config code to server.js

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 
@@ -22,6 +23,16 @@ app.get("/", (req, res) => res.send("Hello"));
 
 // Use routes
 app.use("/api/internships", internships);
+
+// Serve static build if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 // 🔥 up the server port 1024
 const port = process.env.PORT || 1024;
